03634c53d7767529cc3c400a7dd511fe2f041025
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2025-03-07 07:52:16 -0300

    scheduling confirm presence

--- src/routes/Schedulings/Scheduling/index.tsx
+++ src/routes/Schedulings/Scheduling/index.tsx
@@ -1,12 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import NavBarHorizontalOne from "../../../components/NavbarHorizantalOne";
-import NavBarHorizontalTwo from "../../../components/NavbarHorizantalTwo";
-import * as schedulingService from "../../../services/professional-service"
+import * as professionalService from "../../../services/professional-service"
+import * as schedulingService from "../../../services/scheduling-service"
 import { ProfessionalDTO } from "../../../models/professional";
 import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import * as formats from "../../../utils/formats";
+import { SchedulingDTO } from "../../../models/scheduling";
+import { PatientDTO } from "../../../models/patient";
+
+type FormData = {
+    dateHour: string,
+    professional: ProfessionalDTO,
+    patient: PatientDTO,
+    present: boolean,
+    confirmed: boolean,
+    cancel: boolean,
+}
+
 
 export default function Scheduling(){
     const params = useParams();
@@ -15,7 +27,7 @@ export default function Scheduling(){
     const [scheduling, setScheduling] = useState<ProfessionalDTO>();
 
     useEffect(() => {
-        schedulingService.findById(Number(params.profissionalId))
+        professionalService.findById(Number(params.profissionalId))
             .then( 
                 response => {
                     setScheduling(response.data)
@@ -23,13 +35,116 @@ export default function Scheduling(){
             ).catch(() => {
                 navigete("/profissionais");
            });
-    }, [])
+    }, [])    
+ 
+
+    const handleConfirmPresence = (id: number, thisScheduling: SchedulingDTO, thisProfessionalId: number) => {
+        if (!scheduling) return;
+    
+        
+    
+        if(thisScheduling.present == false){
+
+            // Atualiza localmente a lista de agendamentos
+            const updatedSchedulings = scheduling.schedulings.map((item) =>
+                item.id === id ? { ...item, present: true } : item
+            );
+        
+            setScheduling({ ...scheduling, schedulings: updatedSchedulings });
+
+            const updatedData: FormData = {
+                dateHour: thisScheduling.dateHour,
+                professional: { 
+                    id: thisProfessionalId, 
+                    name: '',
+                    specialty: '',
+                    contact: '',
+                    schedulings: []
+
+                }, 
+                patient: { 
+                    id: thisScheduling.patient.id,
+                    name: '',
+                    address: '',
+                    contact: '',
+                    birthDay: '',
+                    reportHistory: []
+                },
+                present: true,
+                confirmed: thisScheduling.confirmed,
+                cancel: thisScheduling.cancel,
+            };
+        
+            console.log("Enviando atualização:", updatedData);
+        
+            // Chama a API para atualizar no backend
+            schedulingService.update(id, updatedData)
+                .then(() => {
+                    
+                })
+                .catch((error) => {
+                    console.error("Erro ao atualizar o agendamento:", error);
+                    alert("Erro ao atualizar o agendamento.");
+                });
+
+        }
+
+        if(thisScheduling.present == true){
+
+            // Atualiza localmente a lista de agendamentos
+            const updatedSchedulings = scheduling.schedulings.map((item) =>
+                item.id === id ? { ...item, present: false } : item
+            );
+        
+            setScheduling({ ...scheduling, schedulings: updatedSchedulings });
+
+            const updatedData: FormData = {
+                dateHour: thisScheduling.dateHour,
+                professional: { 
+                    id: thisProfessionalId, 
+                    name: '',
+                    specialty: '',
+                    contact: '',
+                    schedulings: []
+
+                }, 
+                patient: { 
+                    id: thisScheduling.patient.id,
+                    name: '',
+                    address: '',
+                    contact: '',
+                    birthDay: '',
+                    reportHistory: []
+                },
+                present: false,
+                confirmed: thisScheduling.confirmed,
+                cancel: thisScheduling.cancel,
+            };
+        
+            console.log("Enviando atualização:", updatedData);
+        
+            // Chama a API para atualizar no backend
+            schedulingService.update(id, updatedData)
+                .then(() => {
+                    
+                })
+                .catch((error) => {
+                    console.error("Erro ao atualizar o agendamento:", error);
+                    alert("Erro ao atualizar o agendamento.");
+                });
+
+        }
+        
+    };
+    
 
      return(
     <>
         <div className="main">
             <NavBarHorizontalOne name="Agenda" />  
-            <NavBarHorizontalTwo name={String(scheduling?.name) || ""} icon=""  />             
+            <nav className="navbar-horizontal navbar-horizontal-secondary ">
+                    
+            </nav> 
 
             <div className="p-3 ">
                 <div className="container p-3">
@@ -62,7 +177,11 @@ export default function Scheduling(){
                                                 </Link>                                                
                                                 
                                                 <a 
-                                                    href="" 
+                                                    href={"agendamento/" + i.id} 
+                                                    onClick={(e) => {
+                                                        e.preventDefault();
+                                                        handleConfirmPresence(i.id, i, scheduling.id);
+                                                    }}
                                                     title="Confirmar presenção do paciente"
                                                     className="link-dark me-2">
                                                         {
